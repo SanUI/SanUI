761a1be743583e7ab473d48814bab35a90e1d8cc
SanUI <SanUI@users.noreply.github.com>
2025-09-12 12:33:49 +0200

    Add spell queue window indicator to GCD

--- SanUI/Libs/oUF_GCD.lua
+++ SanUI/Libs/oUF_GCD.lua
@@ -1,7 +1,11 @@
 local _, addon = ...
+local S, C = unpack(SanUI)
 
 local GetTime = GetTime
 local GetSpellCooldown = C_Spell.GetSpellCooldown
+local GetCVar = GetCVar
+local Scale = S.Scale
+local normTex = C.medias.textures.StatusbarNormal
 
 local spellid = 61304
 
@@ -9,22 +13,33 @@ local OnUpdateGCD = function(self)
 	local perc = (GetTime() - self.starttime) / self.duration
 	if perc > 1 then
 		self:Hide()
+		self:SetStatusBarColor(self.orig_r, self.orig_g, self.orig_b, self.orig_a)
 	else
 		self:SetValue(perc)
+		
+		if perc > 1 - self.queue_perc then
+			self:SetStatusBarColor(0,0.8,0)
+		end
 	end
 end
 
 local OnHideGCD = function(self)
+	self.queue_tick:Hide()
  	self:SetScript('OnUpdate', nil)
 end
 
 
 local OnShowGCD = function(self)
 	self:SetScript('OnUpdate', OnUpdateGCD)
+	local spell_queue_window = GetCVar("SpellQueueWindow")/1000
+	self.queue_perc = spell_queue_window / self.duration
+	self.queue_tick:ClearAllPoints()
+	self.queue_tick:SetPoint("RIGHT", self, "RIGHT", -self.queue_perc * self:GetWidth(),0)
+	self.queue_tick:Show()
 end
 
 local Update = function(self, event, unit)
-	if self.GCD then
+	--aif self.GCD then
 		local cdinfo = GetSpellCooldown(spellid)
 
 		local start = cdinfo.startTime
@@ -39,7 +54,7 @@ local Update = function(self, event, unit)
 			self.GCD.duration = dur
 			self.GCD:Show()
 		end
-	end
+	--end
 end
 
 
@@ -49,7 +64,24 @@ local Enable = function(self)
 		self.GCD.starttime = 0
 		self.GCD.duration = 0
 		self.GCD:SetMinMaxValues(0, 1)
+		
+		self.GCD.orig_r, self.GCD.orig_g, self.GCD.orig_b, self.GCD.orig_a = self.GCD:GetStatusBarColor()
+		self.spell_queue_window = GetCVar("SpellQueueWindow")/1000
+		
+		local queue_tick = CreateFrame("Frame", nil, self.GCD)
+		queue_tick:SetWidth(Scale(3))
+		queue_tick:SetHeight(self.GCD:GetHeight())
+		S.CreateBackdrop(queue_tick)
+		queue_tick.SetBackdropColor({0.8,0,0})
+		queue_tick.SetBackdropBorderColor({0.8,0,0})
+		--local tex = queue_tick:CreateTexture()
+		--tex:SetTexture(normTex)
+		--tex:SetColorTexture(1,1,1)
 
+		queue_tick:Hide()
+		--queue_tick.tex = tex
+		self.GCD.queue_tick = queue_tick
+		
 		self:RegisterEvent('ACTIONBAR_UPDATE_COOLDOWN', Update, true)
 		self.GCD:SetScript('OnHide', OnHideGCD)
 		self.GCD:SetScript('OnShow', OnShowGCD)
